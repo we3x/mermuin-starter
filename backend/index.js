ddd40a1f7800f28c23bd276171954c983602ea0d
we3x <veljko.simic98@gmail.com>
2019-06-26 12:38:50 +0200

    connect with mongo

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ip = require('ip');
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 5000;
+
+app.use(bodyParser.json())
+
+mongoose
+ .connect('mongodb://db:27017/db', {useNewUrlParser: true})
+ .then(() => {
+   console.log("[info] Successed connected with api")
+ })
+ .catch(error => {
+   console.log("[error] Connected with db failed "+error)
+ })
+
 
 app.get('/', (req, res) => {
   res.send("<h1>Mermuin-starter</h1>");
